5e6a588e55d4ffe8f0fd77d3bdbeeed7a5789d82
arpit <arpitbhadoriya@hotmail.com>
2021-05-02 16:35:19 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 		}
 	}
 
-	componentDidMount() {
+	componentDidMount() { 
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
 			.then(users => this.setState({ monsters: users }))
